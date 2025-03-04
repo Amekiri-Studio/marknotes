652191499430be89629caab3f1d6487bf0ea5c7b
amekiri <amekiri@Amekiri-PC.>
2025-03-04 21:25:09 +0800

    feat: 路由补充

--- src/routes/tag/index.ts
+++ src/routes/tag/index.ts
@@ -1,4 +1,15 @@
 import { Router } from 'express';
 import Paths from './Paths';
+import TagController from '@src/controllers/TagController';
 
 const router = Router();
+
+router.post(Paths.add, TagController.add);
+
+router.delete(Paths.remove, TagController.remove);
+
+router.get(Paths.list, TagController.listTagWithNote);
+
+router.get(Paths.listByNote, TagController.listTagsByNote);
+
+export default router;
